2b5c5354203b1ef7ad469e6a1891221c84ed85f3
KemalDnmz <108699106+KemalDnmz@users.noreply.github.com>
2023-12-14 13:48:32 +0300

    Add files via upload

---
+++ tour_project/src/Tour.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+function Tour({ id, image, info, name, price, removeTour }) {
+  const [readMore, setreadMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+      <span className="tour-price"> $ {price}</span>
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button className="info-btn" onClick={() => setreadMore(!readMore)}>
+            {readMore ? "less" : "read more"}
+          </button>
+        </p>
+
+        <button
+          className="btn btn-block delete-btn"
+          onClick={() => removeTour(id)}
+        >
+          not interested
+        </button>
+      </div>
+    </article>
+  );
+}
+
+export default Tour;
